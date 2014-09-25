15b7ecb251b3af7478451f3585785a0a98add807
Xiong Yuanjun <bitxiong@gmail.com>
2014-09-25 17:27:35 +0800

    tested with MPI, managed mem, convolution

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <cudnn.h>
 
 #include <iostream>
+#include <mpi.h>
+#include <time.h>
 #include "easylogging++.h"
 using namespace std;
 
@@ -17,24 +19,121 @@ using namespace std;
 // CUDA: various checks for different function calls.
 #define CUDA_CHECK(condition) \
 	/* Code block avoids redefinition of cudaError_t error */ \
-do {\
+	do {\
 	cudaError_t error = condition; \
 	CHECK_EQ(error, cudaSuccess)<< cudaGetErrorString(error); \
-} while (0)
+	} while (0)
 
 // CUDNN: various checks for different function calls.
 #define CUDNN_CHECK(condition) \
 	/* Code block avoids redefinition of cudaError_t error */ \
-do {\
+	do {\
 	cudnnStatus_t error = condition; \
 	CHECK_EQ(error, CUDNN_STATUS_SUCCESS)<< "CUDNN Failed"; \
-} while (0)
+	} while (0)
 
 
 _INITIALIZE_EASYLOGGINGPP
 
+class cudnnTensor{
+private:
+	float* data_;
+	float* diff_;
+
+	void inline setup(){
+		count_ = n_ * c_ * h_ * w_ ;
+		data_ = NULL;
+		diff_ = NULL;
+		CUDNN_CHECK(cudnnCreateTensor4dDescriptor(&descriptor_));
+		CUDNN_CHECK(cudnnSetTensor4dDescriptor(
+			descriptor_,
+			CUDNN_TENSOR_NCHW,
+			CUDNN_DATA_FLOAT,
+			n_,
+			c_,
+			h_,
+			w_
+			));
+		CUDNN_CHECK(cudnnCreateFilterDescriptor(&filter_descriptor_));
+		CUDNN_CHECK(cudnnSetFilterDescriptor(
+			filter_descriptor_,
+			CUDNN_DATA_FLOAT,
+			n_,
+			c_,
+			h_,
+			w_
+			));
+	}
+
+public:
+	int n_;
+	int c_;
+	int h_;
+	int w_;
+
+	int count_;
+
+	cudnnTensor4dDescriptor_t descriptor_;
+	cudnnFilterDescriptor_t filter_descriptor_;
+
+	cudnnTensor(int n, int c, int h, int w){
+		n_ = n;
+		c_ = c;
+		h_ = h;
+		w_ = w;
+		setup();
+	}
+
+	cudnnTensor(cudnnConvolutionDescriptor_t conv_desc_){
+		//Describe output tensor 
+		CUDNN_CHECK(cudnnGetOutputTensor4dDim(conv_desc_, CUDNN_CONVOLUTION_FWD,
+			&n_, &c_,
+			&h_, &w_
+			));
+		setup();
+	}
+
+	inline float* data(){
+		if (!data_){
+			CUDA_CHECK(cudaMallocManaged(&data_, sizeof(float) * count_));
+		}
+		return data_;
+	}
+
+	inline float* diff(){
+
+		if(!diff_){
+			CUDA_CHECK(cudaMallocManaged(&diff_, sizeof(float) * count_));
+		}
+		return diff_;
+	}
+
+	inline cudnnFilterDescriptor_t describe_filter(){
+		return filter_descriptor_;
+	}
+
+	inline cudnnTensor4dDescriptor_t describe(){
+		return descriptor_;
+	}
+
+	inline int count(){
+		return count_;
+	}
+
+};
+
+
 int main(int argc, char** argv){
+
+	MPI_Init(&argc, &argv);
+
+
+	int rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
 	CUDA_CHECK(cudaSetDevice(0));
+	CUDA_CHECK(cudaDeviceReset());
+
 	LOG(DEBUG)<<"cuda started";
 	cudnnHandle_t dnn_handle_;
 	CUDNN_CHECK(cudnnCreate(&dnn_handle_));
@@ -42,53 +141,26 @@ int main(int argc, char** argv){
 	//TODO: add operations here
 
 	// Create input tensor 
-	cudnnTensor4dDescriptor_t input_tensor_;
-	CUDNN_CHECK(cudnnCreateTensor4dDescriptor(&input_tensor_));
 
-	int n_=5, width_=5, height_=5, channel_=5;
-	CUDNN_CHECK(cudnnSetTensor4dDescriptor(
-		input_tensor_,
-		CUDNN_TENSOR_NCHW,
-		CUDNN_DATA_FLOAT,
-		channel_,
-		n_,
-		width_,
-		height_
-		));
-	float* input_data_;
-	float count_input =  n_ * width_ * height_ * channel_;
-	CUDA_CHECK(cudaMallocManaged(&input_data_, 
-		sizeof(float) * count_input));
+	int n_=128, width_=128, height_=128, channel_=3;
+	cudnnTensor* input_blob = new cudnnTensor(n_, channel_, height_, width_);
 
-	for (int i=0;i<count_input; i++){
-		input_data_[i] = 1;
+	float* input_data_ = input_blob->data();
+	for (int i=0;i<input_blob->count(); i++){
+		input_data_[i] = i;
 	}
 	LOG(DEBUG)<<"input tensor intialized";
 
 	/*for (int i=0;i<count_input; i++){
-		LOG(DEBUG)<<input_data_[i];
+	LOG(DEBUG)<<input_data_[i];
 	}*/
 
 	// Create filter descriptor
-	int k_= 5, c_=channel_, h_=3, w_=3;
-	cudnnFilterDescriptor_t filter_desc_;
-	CUDNN_CHECK(cudnnCreateFilterDescriptor(&filter_desc_));
+	int k_= 32, c_=channel_, h_=3, w_=3;
+	cudnnTensor* filter_blob = new cudnnTensor(k_, c_, h_, w_);
 
-	CUDNN_CHECK(cudnnSetFilterDescriptor(filter_desc_,
-		CUDNN_DATA_FLOAT,
-		k_,
-		c_,
-		h_,
-		w_));
-
-	float* filter_data_;
-
-	//allocate filter data
-	float count_filter = k_ * c_ * h_ * w_;
-
-	CUDA_CHECK(cudaMallocManaged(&filter_data_, 
-		sizeof(float) * count_filter));
-	for (int i=0;i<count_filter; i++){
+	float* filter_data_ = filter_blob->data();
+	for (int i=0;i<filter_blob->count(); i++){
 		filter_data_[i] = 1.;
 	}
 	LOG(DEBUG)<<"filter descriptor intialized";
@@ -99,64 +171,56 @@ int main(int argc, char** argv){
 
 	int pad_h_=0,pad_w_=0, u_=1, v_=1, upscalex_=1, upscaley_=1;
 	CUDNN_CHECK(cudnnSetConvolutionDescriptor(
-		conv_desc_, input_tensor_, filter_desc_,
+		conv_desc_, input_blob->describe(), filter_blob->describe_filter(),
 		pad_h_, pad_w_,
 		u_, v_,
 		upscalex_, upscaley_,
 		CUDNN_CONVOLUTION
-	));
+		));
 	LOG(DEBUG)<<"Convolution descriptor initialized";
 
-	int out_n_, out_c_, out_h_, out_w_;
-
-
 	//Describe output tensor 
-	CUDNN_CHECK(cudnnGetOutputTensor4dDim(conv_desc_, CUDNN_CONVOLUTION_FWD,
-		&out_n_, &out_c_,
-		&out_h_, &out_w_
-		));
-
-	int count_out = out_n_ * out_c_ * out_h_ * out_w_;
-	cudnnTensor4dDescriptor_t out_tensor_;
-	CUDNN_CHECK(cudnnCreateTensor4dDescriptor(&out_tensor_));
-
-	CUDNN_CHECK(cudnnSetTensor4dDescriptor(
-		out_tensor_,
-		CUDNN_TENSOR_NCHW,
-		CUDNN_DATA_FLOAT,
-		out_c_,
-		out_n_,
-		out_h_,
-		out_w_
-		));
-
-	float* out_data_;
-	CUDA_CHECK(cudaMallocManaged(&out_data_, 
-		sizeof(float) * count_out));
-	for (int i=0;i<count_out; i++){
-		out_data_[i] = 0.;
-	}
+	cudnnTensor* out_blob = new cudnnTensor(conv_desc_);
 
 	LOG(DEBUG)<<"Output tensor initialized";
 
+	double start_conv = MPI_Wtime();
+
 	//Launch convolution
 	CUDNN_CHECK(cudnnConvolutionForward(dnn_handle_,
-		input_tensor_, reinterpret_cast<const void*>(input_data_),
-		filter_desc_, reinterpret_cast<const void*>(filter_data_),
+		input_blob->describe(), reinterpret_cast<const void*>(input_data_),
+		filter_blob->describe_filter(), reinterpret_cast<const void*>(filter_data_),
 		conv_desc_,
-		out_tensor_, reinterpret_cast<void*>(out_data_),
-		CUDNN_RESULT_ACCUMULATE));
-
+		out_blob->describe(), reinterpret_cast<void*>(out_blob->data()),
+		CUDNN_RESULT_NO_ACCUMULATE));
+	double end_conv = MPI_Wtime();
 	LOG(DEBUG)<<"Convolution Done";
 
 	cudaError_t err = cudaDeviceSynchronize();
-	LOG(INFO)<<"result:";
-	for (int i=0;i<count_out; i++){
-		LOG(INFO)<<out_data_[i];
+
+	float* sum_data_;
+	CUDA_CHECK(cudaMallocManaged(&sum_data_, sizeof(float) * filter_blob->count()));
+	double start_mpi = MPI_Wtime();
+	MPI_Reduce(filter_data_, sum_data_, filter_blob->count(), 
+		MPI_FLOAT, MPI_SUM,
+		0,
+		MPI_COMM_WORLD);
+
+	double end_mpi = MPI_Wtime();
+
+	if (rank ==0){
+		LOG(INFO)<<"result:";
+		/*float* out_data_ = out_blob->data();
+		for (int i= 0; i<out_blob->count();i++){
+			cout<<out_data_[i]<<" ";
+		}*/
+
+		printf("convolution %g ms taken\n", (end_conv-start_conv)*1000.0);
+		printf("mpi %g ms taken\n", (end_mpi-start_mpi)*1000.0);
 	}
 
 	//Release resources
 	CUDNN_CHECK(cudnnDestroy(dnn_handle_));
-	
+	MPI_Finalize();
 	return 0;
 }
